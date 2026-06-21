c9f9c970475ba1406672454b410746ae6fdaab51
Muhammed Kanyi <contact@kanyi.gm>
2026-06-21 14:18:18 +0000

    updated puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,13 +22,13 @@ set :puma_preload_app, true
 set :puma_prune_bundler, true
 set :puma_workers, 2
 set :puma_threads, [0, 16]
-set :puma_bind, "tcp://0.0.0.0:3002"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma.access.log"
 set :puma_error_log, "#{shared_path}/log/puma.error.log"
-set :puma_systemctl_user, nil
 
+# Rails
 set :rails_env, :production
 set :migration_role, :app
 set :assets_roles, [:app]
