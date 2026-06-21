b887861cbc1cb30d4af64083286d3008a09fc825
Muhammed Kanyi <contact@kanyi.gm>
2026-06-21 11:58:34 +0000

    updated the deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,12 +21,13 @@ set :rbenv_ruby, File.read(".ruby-version").strip
 set :puma_preload_app, true
 set :puma_prune_bundler, true
 set :puma_workers, 2
-set :puma_threads, [0, 4]
+set :puma_threads, [0, 16]
 set :puma_bind, "tcp://0.0.0.0:3002"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma.access.log"
 set :puma_error_log, "#{shared_path}/log/puma.error.log"
+set :puma_systemctl_user, nil
 
 set :rails_env, :production
 set :migration_role, :app
