eb787137bd4272bcbdd848db000e63b523e08daa
Muhammed Kanyi <contact@kanyi.gm>
2026-06-21 13:12:41 +0000

    daemonized

--- config/puma.rb
+++ config/puma.rb
@@ -25,6 +25,8 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
+require 'puma/daemon'
+
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
